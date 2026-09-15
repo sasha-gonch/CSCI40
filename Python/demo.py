46914bb68b709f5ac7a64a5eda1278c40111062d
sasha-gonch <278516932+sasha-gonch@users.noreply.github.com>
2026-09-15 16:04:32 -0700

    Project0 First Commit

---
+++ Python/demo.py
@@ -0,0 +1,9 @@
+def is_even(n):
+    return n % 2 == 0
+
+def is_odd(n):
+    return n % 2 != 0
+def absolute_value(n):
+    """Return the absolute value of a number."""
+    return n if n >= 0 else -n
+pp
